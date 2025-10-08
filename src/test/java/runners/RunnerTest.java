09df2ca1bf4d8897478838f60dc395b6e44f89a9
LaMorica Swain <lamorica.swain@gmail.com>
2025-10-07 22:42:20 -0400

    clean up

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -2,10 +2,10 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.TestNGCucumberRunner;
+
 
 @CucumberOptions(
-        features = "src/test/resources/features/UI-Test.feature",
+        features = "src/test/resources/features",
         glue = {"stepDefinitions"},
         plugin = {
             "pretty",
@@ -17,22 +17,22 @@ import io.cucumber.testng.TestNGCucumberRunner;
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
 
-    private TestNGCucumberRunner testNGCucumberRunner;
+//     private TestNGCucumberRunner testNGCucumberRunner;
 
 //     @BeforeClass(alwaysRun = true)
 //     public void setUpClass() {
 //         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
 //     }
-    // @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
-    // public void feature(CucumberFeatureWrapper cucumberFeature) {
-    //     testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-    // }
-    // @DataProvider
-    // public Object[][] features() {
-    //     return testNGCucumberRunner.provideFeatures();
-    // }
-    // @AfterClass(alwaysRun = true)
-    // public void tearDownClass() {
-    //     testNGCucumberRunner.finish();
+//     @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+//     public void feature(CucumberFeatureWrapper cucumberFeature) {
+//         testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+//     }
+//     @DataProvider
+//     public Object[][] features() {
+//         return testNGCucumberRunner.provideFeatures();
+//     }
+//     @AfterClass(alwaysRun = true)
+//     public void tearDownClass() {
+//         testNGCucumberRunner.finish();
     // }
 }
